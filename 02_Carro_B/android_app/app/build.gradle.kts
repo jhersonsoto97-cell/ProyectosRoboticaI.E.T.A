e1c4b9f356806bf1927c615de8a0d6529b4e504c
DENCODE31 <ytermal@unal.edu.co>
2026-08-15 23:33:24 -0500

    chore: version 3.9

--- 02_Carro_B/android_app/app/build.gradle.kts
+++ 02_Carro_B/android_app/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.ieta.smartcar"
         minSdk = 24
         targetSdk = 34
-        versionCode = 30
-        versionName = "3.8"
+        versionCode = 31
+        versionName = "3.9"
     }
 
     signingConfigs {
